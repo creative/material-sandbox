399b57014ec54e944a9aa4affcd0eb33821b0b12
Stephen Esser <Stephen.Esser@Cerner.com>
2021-04-18 11:16:53 -0500

    Add initial application skeleton layout (#2)
    * Add initial application layout
    
    * Add clean install script
    
    * Delete App.test.js
    
    * Revert "Delete App.test.js"
    
    This reverts commit f273662f4f33c73e85459a69bac2595b29abce18.
    
    * Update App.test.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import classNames from 'classnames/bind';
+import styles from './App.module.scss';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import Tooltip from '@material-ui/core/Tooltip';
+
+var cx = classNames.bind(styles);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={cx('application')}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={cx('title')}>
+            Material Sandbox
+          </Typography>
+          <Tooltip title="Github Repository">
+          <Link 
+            href="https://github.com/creative/material-sandbox" 
+            color="inherit" 
+            target="_blank"
+            rel="noopener"
+          >
+            <GitHubIcon />
+          </Link>
+          </Tooltip>
+        </Toolbar>
+      </AppBar>      
+      <div className={cx('main')}>
+        <div className={cx('left')}>Side</div>
+        <div className={cx('middle')}>Middle</div>
+        <div className={cx('right')}>Side</div>
+      </div>
     </div>
   );
 }
